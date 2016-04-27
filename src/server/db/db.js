e2a97af88bb9f77be42008cfb6ae5a8677ea55c0
markditianquin <markbsd@uw.edu>
2016-04-26 17:53:06 -0700

    4th try

--- src/server/db/db.js
+++ src/server/db/db.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/todos');
 
+console.log('Connecting to DB : ', mongoose);
+
 var Todo = mongoose.model('Todo', {
 	task: String,
 	isCompleted: Boolean,
